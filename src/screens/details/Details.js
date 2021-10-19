ed80c0149d84e576bbc97cc821c22bfd4d42ca42
Jaagriti Bisht <jaagritibisht@gmail.com>
2021-10-19 18:49:40 +0530

    Details implemented

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -63,21 +63,7 @@ class Details extends Component {
     }
 
 
-    starClickHandler = (id) => {
-        let starIconList = [];
-        for (let star of this.state.starIcons) {
-            let starNode = star;
-            if (star.id <= id) {
-                starNode.color = "yellow"
-            }
-            else {
-                starNode.color = "black";
 
-            }
-            starIconList.push(starNode);
-        }
-        this.setState({ starIcons: starIconList });
-    }
 
     render() {
         let movie = this.state.movie;
@@ -90,65 +76,45 @@ class Details extends Component {
                     </Typography>
                 </div>
                 <div className="flex-containerDetails">
-                    <div className="leftDetails">
+                    <div>
                         <img src={movie.poster_url} alt={movie.title} />
                     </div>
 
-                    <div className="middleDetails">
+                    <div >
                         <div>
                             <Typography variant="headline" component="h2">{movie.title} </Typography>
                         </div>
                         <br />
                         <div>
                             <Typography>
-                                <span className="bold">Genres: </span> {movie.genres.join(', ')}
+                                <span className="bold">Genres: </span> {movie.genres}
                             </Typography>
                         </div>
-                        <div>
-                            <Typography><span className="bold">Duration:</span> {movie.duration} </Typography>
-                        </div>
+                      
                         <div>
                             <Typography><span className="bold">Release Date:</span> {new Date(movie.release_date).toDateString()} </Typography>
                         </div>
                         <div>
                             <Typography><span className="bold"> Rating:</span> {movie.critics_rating}  </Typography>
                         </div>
-                        <div className="marginTop16">
-                            <Typography><span className="bold">Plot:</span> <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline} </Typography>
-                        </div>
-                        <div className="trailerContainer">
+                      
+                        <div >
                             <Typography>
                                 <span className="bold">Trailer:</span>
                             </Typography>
-                            <YouTube
-                                videoId={movie.trailer_url.split("?v=")[1]}
-                               
-                                onReady={this._onReady}
-                            />
+  
                         </div>
                     </div>
 
-                    <div className="rightDetails">
+                    <div >
         
-                        <div className="bold marginBottom16 marginTop16">
+                        <div >
                             <Typography>
                                 <span className="bold">Artists:</span>
                             </Typography>
                         </div>
                         <div className="paddingRight">
-                            <GridList cellHeight={160} cols={2}>
-                                {movie.artists != null && movie.artists.map(artist => (
-                                    <GridListTile
-                                        className="gridTile"
-                                        onClick={() => this.artistClickHandler(artist.wiki_url)}
-                                        key={artist.id}>
-                                        <img src={artist.profile_url} alt={artist.first_name + " " + artist.last_name} />
-                                        <GridListTileBar
-                                            title={artist.first_name + " " + artist.last_name}
-                                        />
-                                    </GridListTile>
-                                ))}
-                            </GridList>
+                      
                         </div>
                     </div>
                 </div>
