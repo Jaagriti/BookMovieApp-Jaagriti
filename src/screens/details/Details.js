d8f18e3e319271fb02df8ed2d5c3e02235e067b4
Jaagriti Bisht <jaagritibisht@gmail.com>
2021-10-19 18:37:01 +0530

    Details implemented

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -21,27 +21,27 @@ class Details extends Component {
             starIcons: [{
                 id: 1,
                 stateId: "star1",
-                color: "black"
+                
             },
             {
                 id: 2,
                 stateId: "star2",
-                color: "black"
+                
             },
             {
                 id: 3,
                 stateId: "star3",
-                color: "black"
+                
             },
             {
                 id: 4,
                 stateId: "star4",
-                color: "black"
+                
             },
             {
                 id: 5,
                 stateId: "star5",
-                color: "black"
+                
             }]
         }
     }
